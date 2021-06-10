cf06cdcb03ae0bf8407cf8c4855ccc056f1e07ba
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-06-10 09:09:31 +0200

    Connect two DMS endpoints to RDS instances

---
+++ cdc/aws_rds_cdc/dms.tf
@@ -0,0 +1,33 @@
+resource "aws_dms_replication_instance" "dms_instance" {
+  allocated_storage          = 10
+  apply_immediately          = true
+  engine_version             = "3.4.4"
+  multi_az                   = false
+  publicly_accessible        = false
+  replication_instance_class = "dms.t3.micro"
+  replication_instance_id    = "dms-replication-instance"
+  vpc_security_group_ids     = [aws_security_group.default_sg.id]
+}
+
+
+resource "aws_dms_endpoint" "dms_endpoint_source" {
+  database_name = aws_db_instance.db_source.name
+  endpoint_id   = "source-mysql"
+  endpoint_type = "source"
+  engine_name   = "mysql"
+  password      = aws_db_instance.db_source.password
+  port          = aws_db_instance.db_source.port
+  server_name   = aws_db_instance.db_source.address
+  username      = aws_db_instance.db_source.username
+}
+
+resource "aws_dms_endpoint" "dms_endpoint_target" {
+  database_name = aws_db_instance.db_target.name
+  endpoint_id   = "target-postgres"
+  endpoint_type = "target"
+  engine_name   = "postgres"
+  password      = aws_db_instance.db_target.password
+  port          = aws_db_instance.db_target.port
+  server_name   = aws_db_instance.db_target.address
+  username      = aws_db_instance.db_target.username
+}
--- cdc/aws_rds_cdc/rds.tf
+++ cdc/aws_rds_cdc/rds.tf
@@ -1,20 +1,54 @@
+resource "aws_security_group" "default_sg" {
+  name = "rds-sg"
+}
+
+resource "aws_security_group_rule" "postgres" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default_sg.id
+}
+
+resource "aws_security_group_rule" "mysql" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  cidr_blocks       = ["0.0.0.0/0"]
+  protocol          = "tcp"
+  security_group_id = aws_security_group.default_sg.id
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  cidr_blocks       = ["0.0.0.0/0"]
+  protocol          = "tcp"
+  security_group_id = aws_security_group.default_sg.id
+}
+
+
 resource "aws_db_instance" "db_source" {
-  identifier        = "mysql-db-source"
-  allocated_storage = 10
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"
-  name              = var.db_name_mysql
-  username          = var.db_username_mysql
-  password          = var.db_password_mysql
+  identifier             = "mysql-db-source"
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  name                   = var.db_name_mysql
+  username               = var.db_username_mysql
+  password               = var.db_password_mysql
+  vpc_security_group_ids = [aws_security_group.default_sg.id]
 }
 
 resource "aws_db_instance" "db_target" {
-  identifier        = "postgres-db-target"
-  allocated_storage = 10
-  engine            = "postgres"
-  instance_class    = "db.t3.micro"
-  name              = var.db_name_postgres
-  username          = var.db_username_postgres
-  password          = var.db_password_postgres
+  identifier             = "postgres-db-target"
+  allocated_storage      = 10
+  engine                 = "postgres"
+  instance_class         = "db.t3.micro"
+  name                   = var.db_name_postgres
+  username               = var.db_username_postgres
+  password               = var.db_password_postgres
+  vpc_security_group_ids = [aws_security_group.default_sg.id]
 }
